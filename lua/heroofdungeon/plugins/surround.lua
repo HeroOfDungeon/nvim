48a8273a80405109d9ed3be6b8b5a679790ef4ba
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2026-06-09 22:40:24 +0200

    Added surround.nvim

---
+++ lua/heroofdungeon/plugins/surround.lua
@@ -0,0 +1,11 @@
+return {
+	"kylechui/nvim-surround",
+	-- dependencies = {
+	-- 	{
+	-- 		"gregorias/nvim-surround-wk",
+	-- 		config = true,
+	-- 	},
+	-- },
+	version = "^4.0.0",
+	event = "VeryLazy",
+}
