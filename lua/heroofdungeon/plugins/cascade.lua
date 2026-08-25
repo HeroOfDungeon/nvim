f623ba782e9b20fc534972047ca05f2cfa9ee6f5
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2026-08-25 22:52:14 +0200

    Added cascade

---
+++ lua/heroofdungeon/plugins/cascade.lua
@@ -0,0 +1,8 @@
+return {
+  "StefanBartl/cascade.nvim",
+  dependencies = { "StefanBartl/lib.nvim" },
+  ft = { "markdown", "markdown.mdx", "text", "tex", "norg" },
+  opts = {
+    keymaps = { preset = true },
+  },
+}
