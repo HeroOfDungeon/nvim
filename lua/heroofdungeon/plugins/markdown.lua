2400088ca453e77e856fb614f9bdbd90df6189d8
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2026-09-15 21:49:12 +0200

    Adapt custom characters

--- lua/heroofdungeon/plugins/markdown.lua
+++ lua/heroofdungeon/plugins/markdown.lua
@@ -1,24 +1,58 @@
 return {
 	{
-    "MeanderingProgrammer/render-markdown.nvim",
+		"MeanderingProgrammer/render-markdown.nvim",
 
-    config = function ()
-      require("render-markdown").setup({
-        render_modes = { 'n', 'c', 'i' },
-        completions = {
-          lsp = {
-            enabled = true
-          }
-        },
-        sign = {
-          enabled = false
-        },
-        code = {
-          style = "full"
-        }
-      })
-    end
-  },
+		config = function()
+			require("render-markdown").setup({
+				render_modes = { "n", "c", "i" },
+				completions = {
+					lsp = {
+						enabled = true,
+					},
+				},
+				heading = {
+					enabled = false,
+				},
+				sign = {
+					enabled = false,
+				},
+				code = {
+					width = "block",
+					-- left_pad = 2,
+					-- right_pad = 2,
+					border = "thick",
+				},
+				dash = {
+					icon = require("heroofdungeon.core.options").grid.horizontal,
+				},
+				bullet = {
+					icons = require("heroofdungeon.core.options").bullets,
+				},
+				checkbox = {
+					unchecked = {
+						icon = require("heroofdungeon.core.options").checkboxes.unchecked,
+					},
+					checked = {
+						icon = require("heroofdungeon.core.options").checkboxes.checked,
+						scope_highlight = "@markup.strikethrough",
+					},
+				},
+				quote = {
+					repeat_linebreak = true,
+				},
+				pipe_table = {
+					style = "normal",
+					cell = "trimmed",
+					border = require("heroofdungeon.core.options").table_border,
+					alignment_indicator = require("heroofdungeon.core.options").grid.horizontal_special,
+				},
+				indent = {
+					enabled = true,
+					icon = require("heroofdungeon.core.options").grid.vertical,
+				},
+			})
+		end,
+	},
 	-- {
 	-- 	"StefanBartl/markdown.nvim",
 	-- 	dependencies = { "StefanBartl/lib.nvim" },
@@ -33,23 +67,23 @@ return {
 	-- 		require("markdown").setup()
 	-- 	end,
 	-- },
-  -- {
-  -- "StefanBartl/pdfport.nvim",
-  --   dependencies = { "StefanBartl/lib.nvim" },
-  --   cmd = { "PdfPort" },
-  --   opts = {
-  --     default_backend = "pdftotext",
-  --   },
-  --   config = function ()
-  --     -- Oil
-  --     require("pdfport.integrations.oil").setup()
-  --
-  --     -- Telescope
-  --     local pdfport_tel = require("pdfport.integrations.telescope")
-  --
-  --     require("telescope.builtin").find_files({
-  --       previewer = pdfport_tel.previewer({ max_pages = 3 }),
-  --     })
-  --   end
-  -- }
+	-- {
+	-- "StefanBartl/pdfport.nvim",
+	--   dependencies = { "StefanBartl/lib.nvim" },
+	--   cmd = { "PdfPort" },
+	--   opts = {
+	--     default_backend = "pdftotext",
+	--   },
+	--   config = function ()
+	--     -- Oil
+	--     require("pdfport.integrations.oil").setup()
+	--
+	--     -- Telescope
+	--     local pdfport_tel = require("pdfport.integrations.telescope")
+	--
+	--     require("telescope.builtin").find_files({
+	--       previewer = pdfport_tel.previewer({ max_pages = 3 }),
+	--     })
+	--   end
+	-- }
 }
