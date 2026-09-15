f195ec029a09401d07e2b2bd8067756b3b96a153
HeroOfDungeon <170803718+HeroOfDungeon@users.noreply.github.com>
2026-09-15 20:10:42 +0200

    Add :qq as an alternative to :q!

--- lua/heroofdungeon/core/remap.lua
+++ lua/heroofdungeon/core/remap.lua
@@ -3,6 +3,8 @@ vim.g.mapleader = " ";
 vim.keymap.set("n", "dl", "d2l")
 vim.keymap.set("n", "dh", "d2h")
 
+vim.cmd(":cnoreabbrev qq q!")
+
 vim.keymap.set("n", "gj", "<cmd>%!jq .<CR>", { desc = "Format JSON" })
 
 -- Key mappings for tab management
